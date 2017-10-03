8f74873615ec5ea2f35c9ac0c2b8245da980e97d
Auriane de Premilhat <adeprem@vt.edu>
2017-10-03 16:51:33 +0200

    fixed some ugly things

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,4 +2,14 @@
 
 class PhotoUploader < CarrierWave::Uploader::Base
  include Cloudinary::CarrierWave
+
+   process eager: true  # Force version generation at upload time.
+
+   process convert: 'jpg'
+
+  process resize_to_fit: [800, 800]
+
+  version :thumb do
+    process resize_to_fill: [314,194]
+  end
 end
